d3d93f554f55e4614eb003671cc8c89ba2c0b3aa
H A Hridoy <116443064+HAHridoy@users.noreply.github.com>
2025-09-08 00:37:16 +0600

    Service Section

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased container mx-auto px-8`}>
         <Navbar></Navbar>
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import ServicesSection from "./components/ServicesSection";
 
 export default function Home() {
   return (
+    <div>
     <h1>Hello world</h1>
+    <ServicesSection></ServicesSection>
+    </div>
   );
 }
